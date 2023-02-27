a3355d9b6ec73cfa14cab87b1b3f3a9b9fff06da
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-27 10:17:46 -0500

    generated one of my fav quotes randomly

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//load current date on the website
 const getCurrentDate = () => {
   let today = new Date();
   let day = today.getDate();
@@ -22,11 +23,60 @@ const getCurrentDate = () => {
 
 getCurrentDate();
 
-const sendMail = () => {
-  const mailAdress = "someone@example.com";
-  const ccRecipients = "vinamiincanada@gmail.com";
-  const subject = encodeURIComponent("This is my subject");
-  const body = encodeURIComponent(document.getElementById("myText").value);
-  const link = `mailto:${mailAdress}?cc=${ccRecipients}&subject=${subject}&body=${body}`;
-  window.location.href = link;
+//generate one of my favorite quotes randomly
+const generateQuote = function () {
+  const quotes = [
+    {
+      quote: "The secret of getting ahead is getting started",
+      author: "Mark Twain",
+    },
+    {
+      quote: "The best investment you can make is in yourself",
+      author: "Warren Buffett",
+    },
+    {
+      quote: "It always seems impossible until it is done",
+      author: "Nelson Mandela",
+    },
+    {
+      quote:
+        "Success is not final, failure is not fatal: It is the courage to continue that counts",
+      author: "Winston Churchill",
+    },
+    {
+      quote:
+        "A bend in the road is not the end of the road… unless you fail to make the turn",
+      author: "Helen Keller",
+    },
+    {
+      quote: "It’s always too soon to quit!",
+      author: "Norman Vincent Peale",
+    },
+    {
+      quote: "The only guarantee for failure is to stop trying",
+      author: "John C. Maxwell",
+    },
+    {
+      quote:
+        "Perseverance is not a long race; it is many short races one after the other",
+      author: "Walter Elliot",
+    },
+    {
+      quote:
+        "Failure is only the opportunity to begin again, this time more intelligently",
+      author: "Henry Ford",
+    },
+    {
+      quote: "Fall seven times and stand up eight",
+      author: "Japanese Proverb",
+    },
+  ];
+  let arrayIndex = Math.floor(Math.random() * quotes.length);
+  console.log(arrayIndex);
+
+  document.querySelector("#quote").innerHTML = quotes[arrayIndex].quote;
+  console.log(arrayIndex);
+  document.querySelector("#author").innerHTML = quotes[arrayIndex].author;
 };
+
+generateQuote();
